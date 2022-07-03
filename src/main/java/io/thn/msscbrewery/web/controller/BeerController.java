e464bbaa823f62be681b0d489b6ad3d8bf4e5474
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-07-03 15:57:27 -0400

    add beer mvc controller

---
+++ src/main/java/io/thn/msscbrewery/web/controller/BeerController.java
@@ -0,0 +1,21 @@
+package io.thn.msscbrewery.web.controller;
+
+import io.thn.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId")  UUID beerId) {
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+}
