2350bb1d5ff7bce116c504762bba4fa09be8b62b
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-07-04 01:31:42 -0400

    resolve merge conflicts

--- src/main/java/io/thn/msscbrewery/services/CustomerService.java
+++ src/main/java/io/thn/msscbrewery/services/CustomerService.java
@@ -6,4 +6,10 @@ import java.util.UUID;
 
 public interface CustomerService {
     CustomerDto getCustomerById(UUID customerId);
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    CustomerDto updateCustomer(UUID customerId, CustomerDto customerDto);
+
+    void deleteById(UUID customerId);
 }
