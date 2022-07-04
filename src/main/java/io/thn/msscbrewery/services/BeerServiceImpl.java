98c78889dd24a34ff165a7c9ba33759b67c4afdf
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-07-03 22:11:05 -0400

    add handlePost() to beer service/controller and dependencies

--- src/main/java/io/thn/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/io/thn/msscbrewery/services/BeerServiceImpl.java
@@ -16,4 +16,12 @@ public class BeerServiceImpl implements BeerService {
                 .beerStyle("Pale Ale")
                 .build();
     }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
 }
--- src/main/java/io/thn/msscbrewery/web/controller/BeerController.java
+++ src/main/java/io/thn/msscbrewery/web/controller/BeerController.java
@@ -2,10 +2,12 @@ package io.thn.msscbrewery.web.controller;
 
 import io.thn.msscbrewery.services.BeerService;
 import io.thn.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,13 @@ public class BeerController {
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId")  UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDto> handlePost(BeerDto beerDto) {
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
