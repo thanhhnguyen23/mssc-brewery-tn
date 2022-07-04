f98dcf4f13e9cd2f90ed8f22b412a75a83724e62
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-07-03 20:59:05 -0400

    add customer dto; add customer controller; add customer service; add customer service impl

---
+++ src/main/java/io/thn/msscbrewery/web/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package io.thn.msscbrewery.web.controller;
+
+import io.thn.msscbrewery.services.CustomerService;
+import io.thn.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/customer")
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId") UUID customerId){
+        return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+}
